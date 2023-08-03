e31deda3debd29fe0d362b48ef1ed1bfc0a50f77
Jasmine Poliquin <poliquin.jasmine@gmail.com>
2023-08-03 00:37:50 -0400

    Fixed server url

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -13,13 +13,11 @@ export const validatePassword = (password) => {
   return errors;
 };
 
-
-
 export const getServerUrl = () => {
   if (process.env.NODE_ENV) {
     if (process.env.NODE_ENV === "production") {
-      return "https://review-website-backend.onrender.com" 
+      return process.env.REACT_APP_SERVER_URL;
     }
-  };
-  return "http://localhost:8000" 
-}
+  }
+  return "http://localhost:8000";
+};
